4466e4244062dc2fc762130deb8d367365a8bb2f
fgarada1 <81365867+fgarada1@users.noreply.github.com>
2023-02-07 16:32:25 -0600

    Add files via upload

---
+++ app/src/main/java/com/foziea/fetch/FetchRewardsViewModel.kt
@@ -0,0 +1,25 @@
+package com.foziea.fetch
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.foziea.fetch.model.FetchRewardsItem
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class FetchRewardsViewModel: ViewModel() {
+    private val repository: FetchRewardsRepository = FetchRewardsRepository()
+
+    // Good practice not to publicly expose our mutable live data"
+    private val _fetchRewardsItemList = MutableLiveData<List<FetchRewardsItem>>()
+    val fetchRewardsItemList: LiveData<List<FetchRewardsItem>>
+        get() = _fetchRewardsItemList
+
+    fun fetchItems(){
+        viewModelScope.launch(Dispatchers.IO){
+            _fetchRewardsItemList.postValue(repository.fetchItems())
+        }
+
+    }
+}
